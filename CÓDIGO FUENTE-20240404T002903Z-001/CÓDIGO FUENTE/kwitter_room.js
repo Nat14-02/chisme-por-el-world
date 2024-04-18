8cfde3a3e42a392d3620a1c01200d651af617e30
Nat14-02 <129344865+Nat14-02@users.noreply.github.com>
2024-04-17 19:03:09 -0600

    Add files via upload

--- CÓDIGO FUENTE-20240404T002903Z-001/CÓDIGO FUENTE/kwitter_room.js
+++ CÓDIGO FUENTE-20240404T002903Z-001/CÓDIGO FUENTE/kwitter_room.js
@@ -13,13 +13,46 @@ firebase.initializeApp(firebaseConfig);
 //AÑADE TUS ENLACES DE FIREBASE
 
 user=localStorage.getItem("user");
+room=localStorage.getItem("room");
 document.getElementById("welcome").innerHTML="Bienvenido "+user;
 
+function Añadir_sala()
+{
+room=document.getElementById("roomname").value;
+firebase.database().ref("/").child(room).update({
+      porpose:"Saving room name"
+});
+localStorage.setItem("room",room);
+window.location.replace("muro.html");
+}
+function getData() {
+      firebase.database().ref("/" + room).on('value', function (snapshot)
+       { document.getElementById("output").innerHTML = "";
+        snapshot.forEach(function (childSnapshot) 
+        { childKey = childSnapshot.key;
+             childData = childSnapshot.val();
+             if (childKey != "purpose")
+              { firebase_message_id = childKey; 
+                  message_data = childData;
+                   console.log(firebase_message_id); 
+                   console.log(message_data); 
+                   name = message_data['name'];
+                    message = message_data['message'];
+                     like = message_data['like'];
+                      name_with_tag = "<h4> " + name + "<img class='user_tick' src='tick.png'></h4>";
+                       message_with_tag = "<h4 class='message_h4'>" + message + "</h4>";
+                        like_button = "<button class='btn btn-warning' id=" + firebase_message_id + " value=" + like + " onclick='updateLike(this.id)'>";
+                         span_with_tag = "<span class='glyphicon glyphicon-thumbs-up'>Like: " + like + "</span></button><hr>"; 
+                         row = name_with_tag + message_with_tag + like_button + span_with_tag;
+                          document.getElementById("output").innerHTML += row; }
 
-function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
-       Room_names = childKey;
-      //Inicio del código
-
-      //Final del código
       });});}
 getData();
+function logout(){
+      window.location.replace("index.html");
+}
+function redirectToRoomName(name) 
+{ console.log(name);
+       localStorage.setItem("room", name);
+        window.location = "muro.html";
+       }
